4e8a1131d33dbe848e6bc2404e1e6155f4b027f4
ibaricyril2111-cell <ibaricyril2111@gmail.com>
2026-09-23 01:54:29 +0200

    feat: SEO robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin/', '/api/'],
+      disallow: ['/admin/', '/api/', '/connexion', '/mon-compte', '/commande', '/commandes/', '/checkout/', '/panier'],
     },
     sitemap: 'https://powerprimeur.com/sitemap.xml',
   }
